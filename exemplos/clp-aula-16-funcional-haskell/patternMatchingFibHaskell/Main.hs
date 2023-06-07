1b54fbfe3e3fd558631325256718630f6fcd2dd5
Hugo de Paula <hugo@pucminas.br>
2023-06-07 14:02:34 -0300

    Updated Pattern Matching with comments and IO

--- exemplos/clp-aula-16-funcional-haskell/patternMatchingFibHaskell/Main.hs
+++ exemplos/clp-aula-16-funcional-haskell/patternMatchingFibHaskell/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+-- Apenas uma definição da função fib com várias testagem de parâmetros
 fib :: Int -> Int
 fib n 
   | n == 1 = 1
@@ -8,14 +9,19 @@ fib n
   | otherwise = error "Fibonacci nao definido."
 
 
-
-fib2 :: Int -> Int
-fib2 1 = 1
-fib2 2 = 1
-fib2 n = fib2 (n-2) + fib2 (n-1)
-
+-- Função fibonacci com casamento de padrões (pattern matchin)
+-- Múltiplas definições da função fibP, cada definição com um padrão a ser casado no parâmetro de entrada.
+fibP :: Int -> Int
+fibP 1 = 1
+fibP 2 = 1
+fibP n
+  | n > 0 = fibP (n-2) + fibP (n-1)
+  | otherwise = error "Fibonacci nao definido"
 
 main = do
-  putStrLn "\nCasamento de padrões:\n"
-  putStrLn (show (fib 3))
-  putStrLn (show (fib2 3))
+  putStrLn "\n\n-----------------------------------"
+  putStrLn "Fib 4 sem casamento de padrões:\n"
+  putStrLn ("fib 4 = " ++ show (fib 4))
+  putStrLn "\n\n-----------------------------------"
+  putStrLn "Fib 4 com casamento de padrões:\n"
+  putStrLn ("fibP 4 = " ++ show (fibP 4))
